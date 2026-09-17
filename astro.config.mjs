e0bbf393b541329fd772a04e6b4324b61f574ee6
Rodrigo Avilés <32263078+RodrigoAA@users.noreply.github.com>
2026-09-18 01:20:17 +0200

    Configure Astro base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://rodrigoavilesarias.com',
+  site: 'https://rodrigoaa.github.io',
+  base: '/rodrigoavilesarias.com',
   output: 'static',
 });
